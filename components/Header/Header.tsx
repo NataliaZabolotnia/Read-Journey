da20c66c06c721694d97a2ee1220a443e3fb07c5
Nataliia Zabolotnia <zanatav78@gmail.com>
2026-04-07 22:08:49 +0300

    add modal from burger-menu

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,11 @@
+'use client';
 import css from '@/components/Header/Header.module.css';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <section className={css.containerHeader}>
       <svg width="42" height="17" className={css.icon}>
@@ -10,9 +15,37 @@ export default function Header() {
         <svg width="35" height="35" className={css.iconBlock}>
           <use href="/block.svg"></use>
         </svg>
-        <svg width="28" height="28" className={css.menu}>
-          <use href="/menuMob.svg"></use>
-        </svg>
+        <button
+          type="button"
+          aria-label="menu"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg width="28" height="28" className={css.menu}>
+            <use href="/menuMob.svg"></use>
+          </svg>
+        </button>
+        {isOpen && (
+          <div className={css.overlay} onClick={() => setIsOpen(false)}>
+            <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+              <button type="button" onClick={() => setIsOpen(false)}>
+                <svg height="28" width="28" className={css.closeModal}>
+                  <use href="/closeMenu.svg"></use>
+                </svg>
+              </button>
+              <div className={css.nav}>
+                <Link href="/" onClick={() => setIsOpen(false)}>
+                  Home
+                </Link>
+                <Link href="/library" onClick={() => setIsOpen(false)}>
+                  Library
+                </Link>
+              </div>
+              <button type="button" className={css.btnOut}>
+                Log out
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
